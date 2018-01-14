1c01a2baec9e3beb90e3d4ed80c25cf5fbe6de57
MichaelJamesParsons <mjay.parsons@gmail.com>
2018-01-13 20:40:36 -0500

    io

---
+++ src/io.rs
@@ -0,0 +1,27 @@
+use std::io::stdin;
+
+fn main () {
+    'outer: loop {
+        let number: i32 = 10;
+        println!("Pick a number");
+
+        loop {
+            let mut line = String::new();
+
+            let input = stdin().read_line(&mut line);
+            let guess: Option<i32> = input.ok()
+                .map_or(None, |_| line.trim().parse().ok());
+
+            match guess {
+                None => println!("Enter a number"),
+                Some(n) if n == number => {
+                    println!("Yes guessed it");
+                    break 'outer;
+                }
+                Some(n) if n < number => println!("Too low"),
+                Some(n) if n > number => println!("Too high"),
+                Some(_) => println!("Error")
+            }
+        }
+    }
+}
